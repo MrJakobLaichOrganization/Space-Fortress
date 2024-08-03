4e83b071c378aa47ecfe7e3e52e48b0d360e44fe
André Polomat <monstrefou@gmail.com>
2024-08-02 20:25:20 -0400

    Little map

--- src/main.cpp
+++ src/main.cpp
@@ -12,13 +12,37 @@ int main()
 
 	auto tileTexture = sf::Texture::loadFromFile(ASSETS_DIR "/TileMap_SF/tileset_sf.png").value();
 
-	Tilemap tilemap(tileTexture, {64, 64}, {12, 9});
-	BlockGrid grid({12, 9}, &tilemap);
+	Tilemap tilemap(tileTexture, {64, 64}, {32, 32});
+	BlockGrid grid({32, 32}, &tilemap);
 
-	grid.SetBlockType(1, {1, 2});
+	const auto floorTile = 10 * 30 + 10;
 
-	sf::Vector2f viewCenter{};
-	float viewZoom = 1.f;
+	grid.SetBlockType(6 * 30 + 10, {0, 0});
+	grid.SetBlockType(6 * 30 + 12, {6, 0});
+
+	grid.SetBlockType(8 * 30 + 10, {0, 7});
+	grid.SetBlockType(8 * 30 + 12, {6, 7});
+
+	for (std::uint32_t x = 0; x < 5; x++)
+	{
+		grid.SetBlockType(8 * 30 + 17, {x + 1, 0});
+		grid.SetBlockType(9 * 30 + 10, {x + 1, 1});
+		grid.SetBlockType(8 * 30 + 17, {x + 1, 7});
+		grid.SetBlockType(7 * 30 + 14, {0, x + 1});
+		grid.SetBlockType(7 * 30 + 14, {6, x + 1});
+		for (std::uint32_t y = 0; y < 5; y++)
+		{
+			grid.SetBlockType(floorTile, {x + 1, y + 2});
+		}
+	}
+
+	grid.SetBlockType(7 * 30 + 14, {0, 6});
+	grid.SetBlockType(7 * 30 + 14, {6, 6});
+
+	grid.SetBlockType(9 * 30 + 19, {2, 1});
+
+	sf::Vector2f viewCenter{128.f, 128.f};
+	float viewZoom = 1.5f;
 
 	constexpr float minZoom = 0.2f;
 	constexpr float maxZoom = 4.f;
