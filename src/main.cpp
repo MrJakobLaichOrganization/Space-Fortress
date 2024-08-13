249863ac631adcadfdc66c4a668aa98f68cfcdd5
André Polomat <monstrefou@gmail.com>
2024-08-13 02:42:05 -0400

    added a starfield background (#39)
    * Starfield
    
    * Move starfield to its own file
    
    * Formating and fixes
    
    * Moved the starfield update

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,6 @@ int main()
 
     InputManager inputManager;
 
-
     World world{window};
 
     bool showDebug = false;
@@ -82,7 +81,9 @@ int main()
         inputManager.update();
 
         window.clear();
+
         world.render(window);
+
         window.display();
     }
 
