f9ce2a5f7dffc4d75dfea758847e54ff31408d03
lapinozz <monstrefou@gmail.com>
2025-04-06 23:53:57 -0400

    Better crewmate rendering

--- src/entity/attach-entities/crewmate.hpp
+++ src/entity/attach-entities/crewmate.hpp
@@ -33,10 +33,25 @@ public:
     {
         states.transform *= getTransform();
 
+        /*
+        // Debug render
         sf::CircleShape circle(localBounds.size.x / 2.f);
         circle.setFillColor(sf::Color::Yellow);
         circle.setOrigin(-localBounds.position);
         target.draw(circle, states);
+        */
+
+        sf::CircleShape circle(localBounds.size.x / 2.f);
+        circle.setFillColor(sf::Color{64, 154, 201, 255});
+        circle.setOrigin(-localBounds.position);
+        circle.setScale({0.5f, 1.f});
+        target.draw(circle, states);
+
+        circle.setFillColor(sf::Color{214, 202, 146, 255});
+        circle.setScale({0.4f, 0.4f});
+        target.draw(circle, states);
+
+
     }
 
     bool isAdult() const;
@@ -52,6 +67,16 @@ public:
     }
 
     float speed = 50.f;
+    void step(Position direction, sf::Time deltaTime)
+    {
+        move(direction.normalized() * std::min(speed * deltaTime.asSeconds(), direction.length()));
+
+        if (direction.length() > 0)
+        {
+            const auto targetAngle = direction.angle();
+            setRotation(sf::radians(std::lerp(getRotation().asRadians(), targetAngle.asRadians(), 5.f * deltaTime.asSeconds())));
+        }
+    }
 
     static Location posToGridLocation(Position pos, Size tileSize)
     {
@@ -65,8 +90,4 @@ private:
     std::string m_name;
     Gender m_gender;
     Time m_birthTimestamp;
-
-    void step(sf::Time deltaTime);
-    void work(sf::Time deltaTime);
-    void updatePathfinding();
 };
