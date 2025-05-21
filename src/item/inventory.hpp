5e698c8a513b16a7d4b1f1fc1808adda65fa7b17
lapinozz <monstrefou@gmail.com>
2025-05-21 00:36:58 -0400

    Fixed parameters name

--- src/item/inventory.hpp
+++ src/item/inventory.hpp
@@ -13,16 +13,16 @@ class Inventory
 public:
     Inventory(std::uint64_t capacity);
 
-    [[nodiscard]] bool contains(ItemType::Id itemID) const;
+    [[nodiscard]] bool contains(ItemType::Id typeId) const;
     /// @brief Gets the amount of items of the type in inventory
     /// @param itemID id of the item
     /// @return amount of items in inventory
-    [[nodiscard]] int getAmount(ItemType::Id itemID) const;
+    [[nodiscard]] int getAmount(ItemType::Id typeId) const;
     /// @brief Adds items to inventory
     /// @param itemID - ID of the item to add
     /// @param amt - amount of items to add
     /// @return number of items which couldn't fit (weren't added)
-    int add(ItemType::Id itemID, int amt = 1);
+    int add(ItemType::Id typeId, int amt = 1);
     /// @brief Adds items to inventory
     /// @param itemID - ID of the item to add
     /// @param amt - amount of items to add
